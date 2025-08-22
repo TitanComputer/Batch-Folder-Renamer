5849f0c01b1350763331026f912ef65eac10fde3
Titan Computer <39844065+TitanComputer@users.noreply.github.com>
2025-08-22 10:19:20 +0330

    Implement folder browsing functionality and enhance button styling in Batch Folder Renamer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-import tkinter as tk
+from customtkinter import filedialog
 
 
 class BatchFolderRenamer(ctk.CTk):
@@ -22,10 +22,12 @@ class BatchFolderRenamer(ctk.CTk):
         self.path_entry = ctk.CTkEntry(self, height=20, placeholder_text="Target Directory", font=font_bold)
         self.path_entry.insert(0, "C:\\inetpub\\wwwroot\\T2")
         self.path_entry.grid(row=0, column=0, padx=(10, 5), pady=10, sticky="nsew")
+        self.path_entry.configure(state="readonly")
 
         self.browse_btn = ctk.CTkButton(self, text="Browse", height=20)
         self.browse_btn.grid(row=0, column=1, padx=(5, 10), pady=10, sticky="nsew")
         self.browse_btn.configure(font=font_bold)
+        self.browse_btn.configure(command=self.browse_folder)
 
         # Row 2: Frame with Label + Textbox
         self.prefix_container = ctk.CTkFrame(self)
@@ -47,7 +49,15 @@ class BatchFolderRenamer(ctk.CTk):
         # Row 3: Start Button
         self.start_btn = ctk.CTkButton(self, text="Start Process", height=20)
         self.start_btn.grid(row=2, column=0, columnspan=2, padx=10, pady=(0, 10), sticky="nsew")
-        self.start_btn.configure(font=font_bold)
+        self.start_btn.configure(font=ctk.CTkFont(size=16, weight="bold"))
+
+    def browse_folder(self):
+        folder_selected = filedialog.askdirectory(title="Select Target Folder")
+        if folder_selected:
+            self.path_entry.configure(state="normal")
+            self.path_entry.delete(0, "end")
+            self.path_entry.insert(0, folder_selected)
+            self.path_entry.configure(state="readonly")
 
 
 if __name__ == "__main__":
